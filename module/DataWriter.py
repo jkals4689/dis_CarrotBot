72ae08a1316f1be2abeb20c27aaf0eaf71f2cead
jkals4689 <4689asgf@naver.com>
2023-11-13 03:14:28 +0900

    kljhy

--- module/DataWriter.py
+++ module/DataWriter.py
@@ -1,16 +1,27 @@
 import os
 import pandas as pd
 from datetime import datetime as t
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class EventList:
+    jl: str = 'join_left'
+
+
+def get_event(event: str):
+    if event == None:
+        raise ValueError("The parameter cannot contain 'None'.")
+    return getattr(EventList(), event, None)
 
 
 class GeneratorDataFrame:
     def __init__(self, guild: str, event: str):
-        self._EVENTLIST = {'jl': 'join_left'}
-        self.__dataframe = None
-        self.__filename = None
-        self.__path = None
+        self.__dataframe = self.read_dataframe()
+        self.__filename = get_event(event)
+        self.__path = f"./database/{guild.id}_{guild.name}/"
         if event not in list(self._EVENTLIST.keys()):
-            raise ValueError("The event value must be one of 'jl'")
+            raise ValueError("The event value must be one of 'jl'.")
 
     def get_dataframe(self):
         if self.__Event == 'jl':
@@ -23,15 +34,21 @@ class GeneratorDataFrame:
 
     def read_dataframe(self):
         try:
-            self.__dataframe = pd.read_csv(self.__path, encoding='utf-8')
+            return pd.read_csv(self.__path, encoding='utf-8')
         except:
-            print(self.__path,"is not found...")
-            
+            print(self.__path, "is not found...")
+            self.save_dataframe()
 
-    
-    def save_dateframe(self):
-        try: 
-            self.
+    def save_dataframe(self):
+        try:
+            self.__dataframe.to_csv(
+                self.__path+self.__filename, encoding='utf-8', index=False)
+            print("success save data")
+        except:
+            print("error:", self.__path, "does not exist")
+            print("falied save data")
+            os.mkdir(self.__path)
+            self.save_dataframe()
 
     def __del__(self):
         pass
@@ -45,20 +62,9 @@ class CsvWriter(GeneratorDataFrame):
         self.__path = "./database/{}/".format(server)
         self.__data = super().get_dataframe()
 
-    def save_file(self):
-        __path = str(self.get_path())+str(self.__filename) + ".csv"
-        try:
-            self.__data.to_csv(__path,
-                               encoding='utf-8', index=False)
-            print("success save data")
-        except:
-            print(self.get_path(), "is not folder")
-            os.mkdir(self.get_path())
-            self.save_file()
-
     def add_data(self, id: int, name: str, stat: str):
         time = t.now()
-        self.get_data().loc[len(self.__data)-1] = [
+        self.__data.loc[len(self.__data)-1] = [
             time.strftime("%Y.%m.%d"),
             time.strftime("%H:%M:%S"),
             id, name, stat]
