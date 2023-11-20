265170fe4b34e0274b733ca7900f391cd54072d8
jkals4689 <4689asgf@naver.com>
2023-11-21 00:43:04 +0900

    guild데이터파일 확인하는 파일

--- module/datamodule/checkdatafile.py
+++ module/datamodule/checkdatafile.py
@@ -1,5 +1,6 @@
 import nextcord
-import os
+import json
+import asyncio
 import pandas as pd
 from pathlib import Path, WindowsPath, PosixPath
 from module.ErrorModule import write_error_log
@@ -10,55 +11,59 @@ __userdata = ('ID', 'Name', 'Role', 'NickName', 'Warn')
 __settings = (('ID', 'Name'), ('LogCh', 'TxtCh', 'WanrCh', 'Role', 'WarnRole'))
 
 
-class CheckData:
-    def __init__(self, guild):
-        self._path = Path(Path.cwd()/"database" /
-                          "{}_{}".format(guild.id, guild.name))
-
-    def check_guildfolder(self):
-        self._path.mkdir(parents=True, exist_ok=True)
-        print("Success check guild folder.")
-
-    async def check_csvfiles(self):
-        for csvfile in __csv_filenames:
-            if not Path(self._path/"{}".format(csvfile)):
-                try:
-                    dataframe = get_dataframe_type(csvfile)
-                    dataframe.to_csv(self._path+csvfile)
-                except Exception as e:
-                    write_error_log(e)
-                else:
-                    pass
-    
-    async def check_jsonfiles(self):
-        pass
-
-    def get_pathtype(self):
-        if type(self._path) == WindowsPath:
-            name = str(self._path).split("\\")[-1]
-        elif
-    def check_file(self):
-        for filename in __filenames:
-            if not Path(str(self._path)/filename):
-                try:
-                    datafrmae = get_dataframe_type(filename)
-                    datafrmae.to_csv(self._path+filename)
-                except Exception as e:
-                    write_error_log(e)
-                else:
-                    if type(self._path) == WindowsPath:
-                        name = str(self._path).split("\\")[-1]
-                    elif type(self._path) == PosixPath:
-                        name = str(self._path).split("/")[-1]
-                    print("{} in check {}.csv".format(name, filename))
-
-    
-    def __del__(self):
-        pass
-
-
 def get_dataframe_type(filename: str):
     if filename == 'settings':
         return pd.DataFrame(index=__userdata[1], columns=__userdata[0])
     elif filename == 'userdata':
         return pd.DataFrame(columns=__settings)
+
+
+def get_pathType(filename):
+    path = CheckData().get_path()
+    if type(path) == WindowsPath:
+        name = str(path).split("\\")[-1]
+    elif type(path) == PosixPath:
+        name = str(path).split("/")[-1]
+    print("{} in check {}".format(name, filename))
+
+
+async def check_csvfiles():
+    path = CheckData().get_path()
+    for csvfile in __csv_filenames:
+        if not Path(path/"{}".format(csvfile)):
+            try:
+                dataframe = get_dataframe_type(csvfile)
+                dataframe.to_csv()
+            except Exception as e:
+                write_error_log(e)
+            else:
+                get_pathType(csvfile)
+
+
+async def check_jsonfile():
+    path = CheckData().get_path()/jsonfile
+    for jsonfile in __json_filenames:
+        if not Path(path):
+            with open(path, 'w', encoding='utf-8') as outfile:
+                json.dump([], outfile, indent=4)
+
+
+class CheckData:
+    def __init__(self, guild):
+        self._path = Path(Path.cwd()/"database" /
+                          "{}_{}".format(guild.id, guild.name))
+        self.check_guildfolder()
+        asyncio.run(self.check_file())
+
+    def check_guildfolder(self):
+        self._path.mkdir(parents=True, exist_ok=True)
+
+    async def check_file(self):
+        await asyncio.create_task(check_csvfiles())
+        await asyncio.create_task(check_jsonfile())
+
+    def get_path(self):
+        return self._path
+
+    def __del__(self):
+        print("Checking folders and files success.")
