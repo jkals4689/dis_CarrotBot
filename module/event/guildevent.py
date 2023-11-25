0cd33812eb67e1a3d4056144a793feab1591295a
jkals4689 <4689asgf@naver.com>
2023-11-26 03:36:23 +0900

    시발2

--- module/event/guildevent.py
+++ module/event/guildevent.py
@@ -1,3 +1,5 @@
+import logging
+
 import nextcord
 import shutil
 
@@ -5,6 +7,42 @@ from nextcord.ext import commands
 from module.datamodule.checkdatafile import CheckData
 from module.datamodule.userdatawriter import GuildDataWriter
 from pathlib import Path
+
+
+class _Message_print:
+    def __init__(self, event: str, guild: nextcord.Guild = None, before: nextcord.abc.GuildChannel = None,
+                 after: nextcord.abc.GuildChannel = None):
+        self.__guild = guild
+        self.__before = before
+        self.__after = after
+        self.__event = event[0].upper() + event[1:]
+
+    def print_join(self):
+        print("=" * 10 + f"[ {self.__event} ]" + "=" * 10)
+        print(f"Guild Name: {self.__guild.name}\nGuild ID: {self.__guild.id}")
+        print("=" * 10 + f"[ {self.__event} ]" + "=" * 10)
+
+    def print_left(self):
+        print("=" * 10 + f"[ Guild {self.__event} ]" + "=" * 10)
+        print(f"Guild Name: {self.__guild.name}\nGuild ID: {self.__guild.id}")
+        print("=" * 10 + f"[ Guild {self.__event} ]" + "=" * 10)
+
+    def print_guild_update(self):
+        print("=" * 10 + f"[ Guild {self.__event} ]" + "=" * 10)
+        print(f"Before Guild Name: {self.__before.name}\nGuild ID: {self.__before.id}")
+        print(f"After Guild Name: {self.__after.name}\nGuild ID: {self.__after.id}")
+        print("=" * 10 + f"[ Guild {self.__event} ]" + "=" * 10)
+
+    def print_channel_update(self):
+        print("=" * 10 + f"[ Channel {self.__event} ]" + "=" * 10)
+        print(f"Guild : {self.__guild.id}_{self.__guild.name}")
+        print(f'Before Channel Name: {self.__before.name}\nGuild ID: {self.__before.id}')
+        print(f"After Channel Name: {self.__after.name}\nGuild ID: {self.__after.id}")
+        print("=" * 10 + f"[ Channel {self.__event} ]" + "=" * 10)
+
+    def __del__(self):
+        pass
+
 
 class OnGuildEvents(commands.Cog):
     def __init__(self, bot) -> None:
@@ -15,19 +53,42 @@ class OnGuildEvents(commands.Cog):
         CheckData(guild)
         GuildDataWriter(guild).add_userdata()
 
-        print("=" * 10+"[ Guild Join ]"+"="*10)
-        print(f"Guild Name: {guild.name}\nGuild ID: {guild.id}")
-        print("=" * 10+"[ Guild Join ]"+"="*10)
+        _Message_print("join", guild=guild).print_join()
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild: nextcord.Guild):
-        path = Path(Path.cwd()/"database"/f"{guild.id}_{guild.name}")
+        path = Path(Path.cwd() / "database" / f"{guild.id}_{guild.name}")
         shutil.rmtree(path, ignore_errors=True)
 
-        print("=" * 10+"[ Guild Leave ]"+"="*10)
-        print(f"Guild Name: {guild.name}\nGuild ID: {guild.id}")
-        print("=" * 10 + "[ Guild Leave ]" + "=" * 10)
+        _Message_print("leave", guild=guild).print_left()
 
+
+class OnGuildUpdates:
+    def __init__(self, bot):
+        self.bot = bot
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.INFO)
+
+    @commands.Cog.listener()
+    async def on_guild_update(self, before, after):
+        logging.basicConfig(format='[ %(asctime)s ] [ %(levelname)s ] : %(message)s', datefmt="%d/%m/%Y %H:%M:%D")
+
+        path = Path(Path.cwd() / "database" / f"{before.id}_{before.name}")
+        replace_guild_folder = f"{after.id}_{after.name}"
+        path.rename(path.parent / replace_guild_folder)
+
+        self.logger.info(f"[ {before.name} ] [ Channel Update ] : {before.name} => {after.name}")
+        _Message_print("update", before=before, after=after).print_guild_update()
+
+    @commands.Cog.listener()
+    async def on_guild_channel_update(self, before, after):
+        if isinstance(before, nextcord.TextChannel):
+            path = Path(
+                Path.cwd() / "log" / "message" / f"{before.guild.id}_{before.guild.name}" / f"{before.id}_{before.name}_chat.txt")
+            replace_file_name = f"{after.id}_{after.name}_chat.txt"
+            path.rename(path.parent / replace_file_name)
+
+            _Message_print("update", before=before, after=after).print_channel_update()
 
 def setup(bot: commands.Bot):
     bot.add_cog(OnGuildEvents(bot))
--- module/event/loggingmessage.py
+++ module/event/loggingmessage.py
@@ -12,15 +12,24 @@ class MessageLogging(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: nextcord.Message):
-        path = Path(Path.cwd() / "log" / "message" / f"{message.guild.id}_{message.guild.name}"/f"{message.channel.id}")
+        logging.basicConfig(format='[ %(asctime)s ] [ %(levelname)s ] : %(message)s', datefmt="%d/%m/%Y %H:%M:%D")
+
+        path = Path(Path.cwd() / "log" / "message" / f"{message.guild.id}_{message.guild.name}")
         path.mkdir(parents=True, exist_ok=True)
         current_time = datetime.now()
         date = current_time.strftime("%Y.%m.%d")
         time = current_time.strftime("%H:%M:%S")
-        with open(str(path / "message.txt"), 'a', encoding='utf-8') as logging_file:
+
+        with open(str(path / f"{message.channel.id}_{message.channel.name}_chat.txt"), 'a',
+                  encoding='utf-8') as logging_file:
             if not message.author.bot:
                 logging_file.write(
-                    f"[ {date} ] [ {time} ] : [ {message.guild.name} ] [ {message.channel.name} ] : [ {message.author.nick} ] << {message.content}\n")
+                    f"[ {date} ] [ {time} ] : [ {message.author.nick} ] << {message.content}\n")
+
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        logger.info(
+            f"[ {message.guild.name} ] [ {message.channel.name} ] : [ {message.author.name} ] << {message.content}")
 
 
 def setup(bot: commands.Bot):
